38d542c7d57c0cc5feb88427d5533b46f01563d1
Dexterski007 <mbouj007@gmail.com>
2024-03-26 16:24:31 -0700

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,22 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - postorder tree
+ * binary_tree_height - height of tree
  *
  * @tree: pointer to root
- * @func: pointer to function
+ *
+ * Return: 0 or height
  */
 
 
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_h, right_h;
 	if (tree == NULL)
-		return;
-	if (func == NULL)
-		return;
+		return (0);
 
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
+	if (tree->left != NULL)
+		left_h = binary_tree_height(tree->left) + 1;
+	else
+		left_h = 0;
+	if (tree->right != NULL)
+		right_h = binary_tree_height(tree->right) + 1;
+	else
+		right_h = 0;
 
-	func(tree->n);
+	if (left_h > right_h)
+		return (left_h);
+	else
+		return (right_h);
 }
