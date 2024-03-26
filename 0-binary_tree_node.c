4f30d5722a18f60c1748fd93d755fe391a07478a
Dexterski007 <mbouj007@gmail.com>
2024-03-26 11:09:10 -0700

    betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,22 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - insert node
+ *
+ * @parent: parent of the node
+ * @value: value of the node
+ *
+ * Return: pointer to the node
+ */
+
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *child_node;
 
 	child_node = malloc(sizeof(binary_tree_t));
 	if (child_node == NULL)
-		return NULL;
+		return (NULL);
 
 	child_node->n = value;
 	child_node->parent = parent;
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,15 +1,25 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_left - insert node in the left
+ *
+ * @parent: parent of the node
+ * @value: value of the node
+ *
+ * Return: pointer to the node
+ */
+
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *child_node;
 
 	if (parent == NULL)
-		return NULL;
+		return (NULL);
 
 	child_node = binary_tree_node(parent, value);
 	if (child_node == NULL)
-		return NULL;
+		return (NULL);
 
 	if (parent->left != NULL)
 	{
