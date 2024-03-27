908d2321629c743369e62d599faa972af0133fac
Dexterski007 <mbouj007@gmail.com>
2024-03-27 15:31:35 -0700

    queue

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,87 @@
+#include "binary_trees.h"
+
+const binary_tree_t *dequeue(queue_t **queue);
+void enqueue(queue_t **queue, const binary_tree_t *node);
+
+/**
+ * binary_tree_levelorder - find lowest ancestor
+ *
+ * @tree: pointer to root
+ * @func: function
+ */
+
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	queue_t *queue = NULL;
+	const binary_tree_t *current;
+
+	if (func == NULL || tree == NULL)
+		return;
+	enqueue(&queue, tree);
+
+	while (queue != NULL)
+	{
+		current = dequeue(&queue);
+
+		func(current->n);
+
+		if (current->left != NULL)
+			enqueue(&queue, current->left);
+		if (current->right != NULL)
+			enqueue(&queue, current->right);
+	}
+
+}
+
+/**
+ * enqueue - enqueue node to queue
+ *
+ * @queue: queue
+ * @node: node
+ */
+
+
+void enqueue(queue_t **queue, const binary_tree_t *node)
+{
+	queue_t *current, *new = malloc(sizeof (queue_t));
+	if (new == NULL)
+		return;
+
+	new->node = node;
+	new->next = NULL;
+
+	if (*queue == NULL)
+		*queue = new;
+	else
+	{
+		current = *queue;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new;
+	}
+}
+
+/**
+ * dequeue - remove from queue
+ *
+ * @queue: double pointer to queue
+ */
+
+
+const binary_tree_t *dequeue(queue_t **queue)
+{
+	queue_t *temp;
+	const binary_tree_t *node;
+
+	if (*queue == NULL)
+		return (NULL);
+
+	temp = *queue;
+	node = temp->node;
+	*queue = (*queue)->next;
+	free(temp);
+
+	return node;
+
+}
--- binary_trees.h
+++ binary_trees.h
@@ -20,8 +20,19 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
+
+/**
+ * struct queue_s - queue struct
+ * @node: node
+ * @next: next node
+ */
+typedef struct queue_s
+{
+    const binary_tree_t *node;
+    struct queue_s *next;
+} queue_t;
+
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -45,5 +56,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
