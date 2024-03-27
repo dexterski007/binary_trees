a5d3ee01d13ac66a7d0dfd60b7773705d11e6ce9
Dexterski007 <mbouj007@gmail.com>
2024-03-27 15:00:19 -0700

    ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -13,14 +13,23 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				const binary_tree_t *second)
 {
+	binary_tree_t *parent_s, *parent_f;
+
 	if (first == NULL || second == NULL)
 		return (NULL);
 
 	if (first == second)
 		return ((binary_tree_t *)first);
 
-	if (first->parent == second->parent)
-		return (binary_trees_ancestor(first->parent, second->parent));
-	else
-		return (binary_trees_ancestor(first, second->parent));
+	parent_s = second->parent;
+	parent_f = first->parent;
+
+	if (parent_s == first || parent_f == NULL ||
+		(parent_f->parent == NULL && parent_s != NULL))
+		return (binary_trees_ancestor(first, parent_s));
+	if (parent_f == second || parent_s == NULL ||
+		(parent_s->parent == NULL && parent_f != NULL))
+		return (binary_trees_ancestor(second, parent_f));
+
+	return (binary_trees_ancestor(parent_s, parent_f));
 }
